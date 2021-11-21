b2e5d86e657e3b73d04d398b54068080bf0a8569
Pedro Pereira <50621685+pedropereira96@users.noreply.github.com>
2021-11-21 01:14:19 +0000

    New update

--- code/graph.py
+++ code/graph.py
@@ -9,7 +9,6 @@ import time
 import pylab
 import pandas as pd
 import json
-
 import heapq
  
 class Graph:
@@ -19,9 +18,9 @@ class Graph:
         self.V = vertices
         self.graph = []
 
-        self.graph_file = "../content/graph.txt"
-        self.greedy_file = "../content/greedy_algorithm/process_graph_{}.csv"
-        self.aprox_file = "../content/aprox_algorithm/process_graph_{}.csv"
+        self.graph_file = "content/graph.txt"
+        self.greedy_file = "content/greedy_algorithm/process_graph_{}.csv"
+        self.aprox_file = "content/aprox_algorithm/process_graph_{}.csv"
 
     def addEdge(self, u, v):
         edge = (u,v)
@@ -59,42 +58,21 @@ class Graph:
             print(self.graph)
         return self.graph
 
-    def show_graph(self, name):
+    def show_graph(self, name:str):
 
         G = nx.Graph()
-
         G.add_edges_from(self.graph, weight=1)
-        
-
-        
-        val_map={}
-        color=0
-        for x in self.graph:
-            color+=0.1
-            if x[0] not in val_map.keys():
-                val_map[x[0]] = color
-            if x[1] not in val_map.keys():
-                val_map[x[1]] = color
-
-        values = [val_map.get(node, 0.45) for node in G.nodes()]
-        edge_labels=dict([((u,v,),d['weight'])
-                        for u,v,d in G.edges(data=True)])
-        red_edges = [('C','B'),('B','C')]
-        edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
-        pos=nx.spring_layout(G)
-        #labels={x for x in self.graph.keys()}
-        nx.draw_circular(G, with_labels=True)
-        #nx.draw_networkx_labels(G,pos)
-        #nx.draw_networkx_edges(G, pos=pos, width=3.0,edge_color='blue', style='solid', edge_vmin = 2, min_source_margin = 3)
-        
-        plt.savefig("../content/generated_graph_"+str(name)+".png")
-
+        nx.draw(G, with_labels=True)
+        plt.savefig("content/graphs/generated_graph_"+str(name)+".png")
+        plt.tight_layout()
+        #plt.show()
         #pylab.show()
+        plt.close()
+
 
     def adjacency_table(self):
         #self.graph = array( ( 'A','B' ))
         #adjacency_table = {'A':{ neighbors: ['B','C'] ,  degree:2 },  ...   }
-
         #criar [(u,v), (v,u)]
         graph_data = self.graph
         graph_data.extend( [(v,u) for u,v in self.graph] )
@@ -108,7 +86,7 @@ class Graph:
         
         return adjacency   
   
-    410
+
     def show_results(self, adjacency, stage, option, name):
         
         result = {'':[], 'vector':[],'neighbors':[], 'degree':[],'visited':[], 'option': []}
@@ -155,13 +133,10 @@ class Graph:
 
 
     def greedy_algorithm(self,name):
-
         adjacency = self.adjacency_table()
-
         count=1
         final_result = []
-
-        #Napenas corre os que ainda não foram visitados
+        #apenas corre os que ainda não foram visitados
         while False in tuple((adjacency[key]['visited']) for key in adjacency):
             #Para procurar a melhor opção para o vértice
             #  (Conter mais número de arestas(vértices associados) e que ainda não tenham sido visitados)
@@ -170,49 +145,21 @@ class Graph:
                 if adjacency[key]['degree'] > max and adjacency[key]['visited'] == False:
                     term = key
                     max = adjacency[key]['degree']
-
-            #Apenas para mostrar informação do funcionamento 
-            self.show_results(adjacency, count, option = term, name=name)  
-            
             #Se o vértice ainda não foi visitado, então vamos visita-lo
             if not adjacency[term]['visited']:
                 adjacency[term]['visited'] = True
                 final_result.append(term)
-
                 #Iniciar remoção dos termo atual no vértices associados
                 to_remove_list = adjacency[term]['neighbors']
-
                 for x in to_remove_list:
                     #remover
-
                     adjacency[x]['neighbors'].remove(str(term))
-
                     #atualizar grau
                     adjacency[x]['degree'] = len(adjacency[x]['neighbors'])
-
                     #caso fique sem vetores associados, coloco como visitado para não voltar a verificar
                     if len(adjacency[x]['neighbors']) == 0:
                         adjacency[x]['visited'] = True
-              
             count += 1
-
-
-            result = {'':[], 'vector':[],'neighbors':[], 'degree':[],'visited':[], 'option': []}
-            result[''].append('')
-            result['vector'].append('')
-            result['neighbors'].append('')
-            result['degree'].append('')
-            result['visited'].append('')
-            result['option'].append('')
-
-            result[''].append('')
-            result['vector'].append('')
-            result['neighbors'].append('Result :')
-            result['degree'].append(final_result)
-            result['visited'].append('')
-            result['option'].append('')
-
-
         return final_result
    
 
@@ -244,7 +191,8 @@ class Graph:
             arbitrary = random.choice(list(E.keys()))
             u, v = tuple(E[str(arbitrary)])
             aux=[]
-            C.append(E[str(arbitrary)])
+            C.append(E[arbitrary][0])
+            C.append(E[arbitrary][1])
             aux.extend(E)
             for q in aux:
                 x,y = tuple(E[q])
@@ -269,42 +217,32 @@ class Graph:
         
 
     def greedy_min_vertex_cover(self):
-
         graph = self.adjacency_table1()
-
         queue: list[list] = []
-
-
         for key, value in graph.items():
             # O(log(n))
             heapq.heappush(queue, [len(value), (key, value)])
-
         # chosen_vertices = set of chosen vertices
         chosen_vertices = set()
-
-
         # while queue isn't empty and there are still edges
         #   (queue[0][0] is the rank of the node with max rank)
         while queue and queue[0][0] != 0:
             # extract vertex with max rank from queue and add it to chosen_vertices
             argmax = heapq.heappop(queue)[1][0]
-            
             chosen_vertices.add(argmax)
-            
             # Remove all arcs adjacent to argmax
-            for elem in queue:
-                
+            for elem in queue:  
                 # if v haven't adjacent node, skip
                 if elem[0] == 0:
                     continue
                 # if argmax is reachable from elem
-                # remove argmax from elem's adjacent list and update his rank
-                
+                # remove argmax from elem's adjacent list and update his rank        
                 if argmax in elem[1][1]:
                     index = elem[1][1].index(argmax)
                     del elem[1][1][index]
-                    elem[0] -= 1
-           
+                    elem[0] -= 1     
             # re-order the queue
-            heapq.heapify(queue)
+            heapq.heapify(queue)      
         return chosen_vertices
+
+
--- code/main.py
+++ code/main.py
@@ -1,85 +1,127 @@
 
+from matplotlib.colors import rgb2hex
 from graph import Graph
 from indexer import Indexer
 import sys
 import getopt
 import random
 import time
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import pylab
+import pandas as pd
 
 
-def usage():
-    print("Usage: python3 main.py ")
+    
+def random_graph(vertices_number: int) :
+    graph=[]
+
+    # for each couple of nodes, add an edge from u to v
+    # if the number randomly generated is greater than probability probability
+    for i in range(vertices_number):
+        for j in range(i + 1, vertices_number):
+            if random.random() < 0.5:
+                graph.append((i,j))
+    return graph
+
+def process(nvertexs, graph,  approx, vertexs, greedy ):
+    
+    G = Graph(nvertexs)
+    for (u,v) in graph:
+        G.addEdge(str(u),str(v))
+
+    G.show_graph(str(nvertexs))
+
+    #write graph on txt file
+    G.import_graph_to_txt()
+    
+    vertexs.append(str(nvertexs))
+    print("******************************************************")
+    print("Graph with {} vertices".format(nvertexs))
+    print()
+    G.print_graph(True)
+    print()
+    
+    #Approx Vertex Cover Algorithm
+    print("Approx Vertex Cover Algorithm")
+    tic = time.time()
+    print("Resultado: " + str(G.aprox_algorithm(name=nvertexs)))
+    t_approx =time.time() - tic
+    approx.append(t_approx)
+    print("Tempo de execução: " + str(t_approx))
+    print()
+
+    
+    #Greedy Algorithm
+    print("Greedy Algorithm")
+    tic = time.time()
+    print("Resultado: " + str(G.greedy_algorithm(name=nvertexs)))
+    t_greedy =time.time() - tic
+    greedy.append(t_greedy)
+    print("Tempo de execução: " + str(t_greedy))
+    print()
+    
+    
+    return approx, vertexs, greedy
+
+       
+def create_times_graph(points, approx ,greedy):
+    # x-coordinates of left sides of bars
+    bar_width = 0.3
+    r1 = np.arange(len(approx))
+    r2 = [x + bar_width for x in r1]
+    r3 = [x + bar_width for x in r2]
+    left = points
+    
+    # heights of bars
+    height = nvertexs
+    
+    # labels for bars
+    tick_label = nvertexs
+    
+    plt.figure(figsize=(10,5))
+
+    # plotting a bar chart
+    plt.bar(r1, approx,  width = bar_width, color = '#6A5ACD', tick_label="Approx Min Vertex Cover")
+    plt.bar(r2, greedy,  width = bar_width, color = '#6495ED', tick_label="Greedy Algorithm")
+    # naming the x-axis
+    plt.xlabel('n Vertexs')
+    plt.xticks([r + bar_width for r in range(len(points))],[str(r) + " vertexs" for r in range(len(points))])
+    # naming the y-axis
+    plt.ylabel('Times')
+    # plot title
+    plt.title("Min Vertex Cover")
+    
+    # function to show the plot
+    plt.legend(('Approx Min Vertex Cover','Greedy Algorithm'))
+    plt.show()
+    plt.close()
+
 
 if __name__ == "__main__":
-    random.seed(106346)
-    """g = Graph(8)
-    g.addEdge('1', '2')
-    g.addEdge('1', '4')
-    g.addEdge('2', '5')
-    g.addEdge('2', '3')
-    g.addEdge('5', '6')
-    g.addEdge('3', '6')
-    g.addEdge('3', '7')
-    g.addEdge('3', '8')"""
+    random.seed(106346) 
+
+    approx = []
+    vertexs = []
+    greedy = []
+    greedy_p = []
+
+    #Create x graphs with x vertexs
+    for nvertexs in range(2,10):
+
+        #generate random graph
+        graph = random_graph(nvertexs)
+        while graph == []:
+            graph = random_graph(nvertexs)
 
 
-    """g = Graph(8)
-    g.addEdge('1', '2')
-    g.addEdge('1', '4')
-    g.addEdge('2', '5')
-    g.addEdge('2', '3')
-    g.addEdge('5', '6')
-    g.addEdge('3', '6')
-    g.addEdge('3', '7')
-    g.addEdge('3', '8')
-    print(g.greedy_min_vertex_cover())"""
-    for nvertexs in range(1,10):
-        print("/////////////GRAPH N {} ///////////////////".format(nvertexs))
-        g = Graph(nvertexs)
-        
-        #Escolher vértice e verificações se já existem
-        for v in range(0,nvertexs): 
-            
-            x = str(random.randrange(1,9))
-            y = str(random.randrange(1,9))
+        approx, vertexs, greedy = process(nvertexs , graph, approx, vertexs, greedy )
 
-            while (((x,y) in g.print_graph(False)) or ((y,x) in g.print_graph(False))) or x==y :
-                x = str(random.randrange(1,9))
-                y = str(random.randrange(1,9))
+    print(approx)
+    print(greedy_p)
+    print(greedy)
+    print(vertexs)
 
-            
-            g.addEdge( x , y )
-        
-        g.print_graph(True)    
-        g.show_graph(name=nvertexs)
+    create_times_graph(vertexs, approx, greedy)
 
-        #escreve o grafico no ficheiro txt
-        g.import_graph_to_txt()
-
-        #print(g.greedy_min_vertex_cover())
-        print()
-        print("Approx Vertex Cover Algorithm")
-        tic = time.time()
-        print("Resultado: " + str(g.aprox_algorithm(name=nvertexs)))
-        print("Tempo de excução: " + str(time.time() - tic))
-        print()
-        print("Greedy Algorithm PROF")
-        tic = time.time()
-        print("Resultado: " + str(g.greedy_min_vertex_cover()))
-        print("Resultado: " + str(g.greedy_min_vertex_cover()))
-        print("Resultado: " + str(g.greedy_min_vertex_cover()))
-        print("Tempo de excução: " + str(time.time() - tic))
-        print()
-        
-        print("Greedy Algorithm")
-        tic = time.time()
-        print("Resultado: " + str(g.greedy_algorithm(name=nvertexs)))
-        print("Resultado: " + str(g.greedy_algorithm(name=nvertexs)))
-        print("Resultado: " + str(g.greedy_algorithm(name=nvertexs)))
-        print("Tempo de excução: " + str(time.time() - tic))
-        print()
-        print()
-    #i = Indexer()
-    #i.create_file()
-    #i.write_file(g.print_graph())
-    #g.printVertexCover()
